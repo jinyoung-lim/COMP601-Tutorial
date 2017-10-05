3aaaa05c8db9e42e60e2faf68a2d3dffa6ea3680
jlim2 <jlim2@macalester.edu>
2017-10-04 21:04:11 -0500

    Started the "Pebble Solitaire" problem from Open Kattis. Declared functions according to my pseudocode. Documentation needed.

---
+++ pebbleSolitaire/PebbleSolitaire.py
@@ -0,0 +1,21 @@
+""" File: PebbleSolitaire.py
+Author: JJ Lim
+Date: 10/4/2017
+
+A class dedicated to solve "Pebble Solitaire" problem from Open Kattis (https://open.kattis.com/problems/pebblesolitaire).
+"""
+
+def main():
+    return
+
+def getMinPebblesAll(num, boards):
+    return
+
+def getMinPebbles(board):
+    return
+
+def pebbleCharToInt():
+    return
+
+if __name__ == "__main__":
+    main()
