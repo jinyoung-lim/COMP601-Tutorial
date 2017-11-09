f00a146ab4f0b608d1c318b8249baa03c592bf9b
jlim2 <jlim2@macalester.edu>
2017-11-08 19:58:07 -0600

    Successfully implemented dfs algorithm to get the number of minimum cavities possible for each board following Pebble Solitaire rules. Added documentation.

--- pebbleSolitaire/PebbleSolitaire.py
+++ pebbleSolitaire/PebbleSolitaire.py
@@ -5,17 +5,10 @@ Date: Oct 2017 - Nov 2017
 A class dedicated to solve "Pebble Solitaire" problem from Open Kattis (https://open.kattis.com/problems/pebblesolitaire).
 """
 
-# WORK WITH DFS
-#     - cavity -> look at possible moves -> move! and make copies!
-#     - compare copies in the end!
-
 def main():
     boards = readPebbleFile("pebblesolitaire.in")
-    numGames = boards.pop(0)
     for board in boards:
-        print(board)
-        print(getMinPebbles(board))
-        print()
+        print(dfsPebbles(board).count(1))
 
 
 def dfsPebbles(board):
@@ -26,148 +19,57 @@ def dfsPebbles(board):
     '''
     boardCopy = board
 
-
-    # preds = {}
-    # visited = []
-    # frontierStack = []
-    #
-    # preds[startInd] = -1
-    # visited.append(startInd)
-    # frontierStack.append(startInd)
-
-
-    cavityInd = []
-    for i in range(0, len(board)-1):
-        if board[i] == 1:
-            cavityInd.append(i)
-
-    visited = []
+    cavityInd = getCavityInds(board)
 
     while(cavityInd):
         currInd = cavityInd.pop()
-        visited.append(currInd)
         if (currInd < len(boardCopy)-3 and boardCopy[currInd + 1] == 1 and boardCopy[currInd + 2] == 0):
-            # found the path to move right
-            boardCopy[currInd] = 0
-            boardCopy[currInd + 1] = 0
-            boardCopy[currInd + 2] = 1
-            # update the cavityInd list
-            if (currInd + 1 in cavityInd):
-                cavityInd.remove(currInd + 1)
-            cavityInd.append(currInd + 2)
+            moveRight(boardCopy, currInd)           # found the path to move right
+            cavityInd = getCavityInds(boardCopy)    # update the cavityInd list
 
         if (currInd > 2 and boardCopy[currInd - 1] == 1 and boardCopy[currInd - 2] == 0):
-            # found the path to move right
-            boardCopy[currInd] = 0
-            boardCopy[currInd - 1] = 0
-            boardCopy[currInd - 2] = 1
-
-            # update the cavityInd list
-            if (currInd - 1 in cavityInd):
-                cavityInd.remove(currInd - 1)
-            cavityInd.append(currInd - 2)
-        # else:
-        #     neighs = [boardCopy[currInd - 1], boardCopy[currInd + 1]]
-        #     for neigh in neighs:
-        #         if (neigh not in visited):
-        #             visited.append(neigh)
-        #             # preds[neigh] = currInd
-        #             # frontierStack.append(neigh)
+            moveLeft(boardCopy, currInd)            # found the path to move right
+            cavityInd = getCavityInds(boardCopy)    # update the cavityInd list
 
     return boardCopy
 
-def getMinPebblesAll(num, boards):
-    pass
 
-def getMinPebbles(board):
-    boardCopy = board
-    minCavities = boardCopy.count(1)
-    # while (dfsPebbles(boardCopy)):
-    #     boardCopy = dfsPebbles(boardCopy)
-    #     print(boardCopy)
-    #     numCavities = boardCopy.count(1)
-    #     if (numCavities < minCavities):
-    #         minCavities = numCavities
-    return dfsPebbles(board).count(1)
-    # numMoves = 12
-    # for cavity in board:
-    #     print(cavity, end=" ")
-    # print()
-    # while(numMoves >= 0):
-    #     for cavity in board:
-    #         print(cavity)
-    #         checkLeft = checkEligibleMoveLeft(board, cavity)
-    #         checkRight = checkEligibleMoveRight(board, cavity)
-    #         print(checkRight)
-    #         if checkLeft == checkRight:
-    #             if checkRight >= 0:
-    #                 moveLeft(board, cavity)
-    #                 numMoves -= 1
-    #             else:
-    #                 numMoves -= 1
-    #
-    #         elif checkLeft > checkRight:
-    #             moveLeft(board, cavity)
-    #             numMoves -= 1
-    #         else:
-    #             moveRight(board, cavity)
-    #             numMoves -= 1
-    # for cavity in board:
-    #     print(cavity, end=" ")
-    # print()
-    # return board.count(1)
+def getCavityInds(board):
+    """ Get indexes of cavities in board list.
+    :param board: list of integers containing information about the pebble solitaire board. Empty pebbles are noted
+     with 0, cavities are noted with 1.
+    :return: a list of integers containing indexes of cavities
+    """
+    cavityInd = []
+    for i in range(0, len(board)):
+        if board[i] == 1:
+            cavityInd.append(i)
+    return cavityInd
+
 
 def moveLeft(board, cavityPos):
+    """ Move a cavity left.
+    :param board: list of integers containing information about the pebble solitaire board. Empty pebbles are noted
+     with 0, cavities are noted with 1.
+    :param cavityPos: index of the cavity to be moved left
+    :return:
+    """
     board[cavityPos] = 0
     board[cavityPos-1] = 0
     board[cavityPos-2] = 1
 
+
 def moveRight(board, cavityPos):
+    """ Move a cavity right.
+    :param board: list of integers containing information about the pebble solitaire board. Empty pebbles are noted
+     with 0, cavities are noted with 1.
+    :param cavityPos: index of the cavity to be moved right
+    :return:
+    """
     board[cavityPos] = 0
     board[cavityPos+1] = 0
     board[cavityPos+2] = 1
 
-def checkEligibleMoveLeft(board, cavityPos):
-    """Check if a given cavity can move to left, and if so, if it can move the next turn (a winning move)
-    :param board: an integer array of length 12 containing pebble solitaire board information (empty cavities are noted
-    with 0 and cavities with 1)
-    :param cavityPos: position of the cavity on the board
-    :return: a negative number(-1) if left move is not eligible; 0 if eligible but not a winning move; a positive
-    number(1) if a winning left move
-    """
-    if cavityPos <= 1: #check for boundary
-        return -1
-
-    if board[cavityPos-1] == 1 and board[cavityPos-2] == 0:
-        return 1 if (cavityPos >= 3 and board[cavityPos-3] == 1) else 0 #check for winning move
-        # if cavityPos <= len(board) - 4 and board[cavityPos+3] == 1: #check for winning move
-        #     return 2
-        # else:
-        #     return 1
-
-    print("left")
-    return -1
-
-
-def checkEligibleMoveRight(board, cavityPos):
-    """Check if a given cavity can move to right, and if so, if it can move the next turn (a winning move)
-    :param board: an integer array of length 12 containing pebble solitaire board information (empty cavities are noted
-    with 0 and cavities with 1)
-    :param cavityPos: position of the cavity on the board
-    :return: a negative number(-1) if right move is not eligible; 0 if eligible but not a winning move; a positive
-    number(1) if a winning right move
-    """
-    if cavityPos >= len(board) - 2:  # check for boundary
-        return -1
-
-    if board[cavityPos + 1] == 1 and board[cavityPos + 2] == 0:
-        return 1 if ((cavityPos <= len(board) - 4) and (board[cavityPos + 3] == 1)) else 0 #check for winning move
-        # if cavityPos <= len(board) - 4 and board[cavityPos+3] == 1: #check for winning move
-        #     return 2
-        # else:
-        #     return 1
-    # print("right")
-    return -1
 
 def readPebbleFile(file):
     """Reads in the file with game information.
@@ -180,18 +82,15 @@ def readPebbleFile(file):
     """
     with open(file) as f:
         boards = []
-        cavities = []
         for line in f:
             board = []
-            if len(line) == 2:
-                boards.append(int(line[0])) #number of games (boards) to be played
-            else:
+            if len(line) != 2:  #actual boards, NOT number of games (boards) to be played
                 for char in line[0:12]:
                     board.append(0 if char == "-" else 1)
-
                 boards.append(board)
 
     return boards
+
 
 if __name__ == "__main__":
     main()
